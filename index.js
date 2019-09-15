b1e670e5215b47bdfe43b8389a4d0440c8aa7645
searching-form-5732 <kevin.downs04@gmail.com>
2019-09-15 10:08:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 function theBeatlesPlay(musicians, instruments){
   var array = [];
   for( var i = 0; i <= musicians.length; i++){
-    var temp = `${musicians} plays ${instruments}`;
+    var temp = `${musicians[i]} plays ${instruments[i]}`;
     array.push(temp);
   }
   return array;
